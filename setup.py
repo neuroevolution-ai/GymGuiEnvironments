890d7831e5470f299a2c3ff04ee2a81b1d64a067
Patrick Deubel <patrick.deubel@gmail.com>
2021-12-22 00:59:18 +0100

    Added requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-setup(name="gym_gui_environments",
-      version="0.0.1",
-      install_requires=["gym"]
-      )
+setup(
+    name="gym_gui_environments",
+    version="0.0.1",
+    install_requires=[
+          "gym",
+          "pyside6",
+          "coverage",
+          "numpy"
+    ]
+)
