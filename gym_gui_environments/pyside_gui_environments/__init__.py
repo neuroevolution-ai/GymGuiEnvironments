ba75e71f6108c875186d9f42db9fc91e26790752
Patrick Deubel <patrick.deubel@gmail.com>
2021-12-22 23:04:12 +0100

    Fixed coverage.py not finding module to measure, because paths are different when using pip install
    - Changed sources to match package not path
    - Added wildcards to the front of exclude paths so the excluded files can be found

--- gym_gui_environments/pyside_gui_environments/__init__.py
+++ gym_gui_environments/pyside_gui_environments/__init__.py
@@ -1,3 +1,1 @@
-# Required for coverage.py to recognize the source files, seems not to interfere with something
-import gym_gui_environments.pyside_gui_environments.src
 from gym_gui_environments.pyside_gui_environments.gui_env import GUIEnv, GUIEnvRandomClick, GUIEnvRandomWidget
