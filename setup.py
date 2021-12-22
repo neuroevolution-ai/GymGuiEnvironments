a02ebf6cb469e19d3c217cca31b226e011b2668f
Patrick Deubel <patrick.deubel@gmail.com>
2021-12-22 13:36:55 +0100

    Fixed not copying non-python files
    - Added MANIFEST.in that specifies the files to be copied
    - Added include_package_data=True in setup.py which specifies that the MANIFEST.in shall be used

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version="0.0.1",
     author="Patrick Deubel",
     packages=setuptools.find_packages(),
+    include_package_data=True,  # Ignores package_data argument and uses files listed in MANIFEST.in instead
     install_requires=[
           "gym",
           "pyside6",
