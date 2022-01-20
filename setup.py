0bdff3b1b31f93e3c494e319bbd497bf497d62b9
Patrick Deubel <patrick.deubel@gmail.com>
2022-01-20 13:38:31 +0100

    Added a test if the required fonts are installed on the system
    - Checks for all fonts to be installed in setup.py, if one is missing an error is raised and the package is not installed

--- setup.py
+++ setup.py
@@ -2,6 +2,21 @@ import setuptools
 
 from gym_gui_environments import __version__ as version
 
+
+def test_font_installation():
+    import subprocess
+    from gym_gui_environments.pyside_gui_environments.src.backend.text_printer import FONTS
+
+    for font in FONTS:
+        output = subprocess.run(["fc-match", font], capture_output=True, text=True).stdout
+
+        if font not in output:
+            raise ModuleNotFoundError(f"Could not find the font '{font}' on this system, please install it using your "
+                                      "system package manager and then try installing this package again!")
+
+
+test_font_installation()
+
 setuptools.setup(
     name="gym_gui_environments",
     version=version,
