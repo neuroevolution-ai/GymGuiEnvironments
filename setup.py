2be9c6a12ada0b332a868e78f8ca56a7b5263024
Patrick Deubel <patrick.deubel@gmail.com>
2022-04-11 23:35:54 +0200

    Added "wheel" as a requirement for the installation, bumped version to 1.0
    - wheel seems to be neede when installing

--- setup.py
+++ setup.py
@@ -19,11 +19,12 @@ test_font_installation()
 
 setuptools.setup(
     name="gym_gui_environments",
-    version="0.0.8",
+    version="1.0.0",
     author="Patrick Deubel",
     packages=setuptools.find_packages(),
     include_package_data=True,  # Ignores package_data argument and uses files listed in MANIFEST.in instead
     install_requires=[
+          "wheel",
           "gym",
           "pyside6",
           "coverage",
